4e79e4ae3de9c1d728ddae3dce8c4da95e781a65
Valentin Leon <valentin@leon.click>
2026-08-20 11:34:56 -0700

    Guard fakecygpty use on Windows

--- vterm.el
+++ vterm.el
@@ -800,8 +800,10 @@ Exceptions are defined by `vterm-keymap-exceptions'."
            :name "vterm"
            :buffer (current-buffer)
            :command
-           `(,fakecygpty-program "/bin/sh" "-c"
-                                 ,(format
+           `(,@(when (eq system-type 'windows-nt)
+                   (list fakecygpty-program))
+              "/bin/sh" "-c"
+              ,(format
                "stty -nl sane %s erase ^? rows %d columns %d >/dev/null && exec %s"
                ;; Some stty implementations (i.e. that of *BSD) do not
                ;; support the iutf8 option.  to handle that, we run some
@@ -831,7 +833,8 @@ Exceptions are defined by `vterm-keymap-exceptions'."
             (lambda () (interactive)
               (user-error "You cannot change major mode in vterm buffers")) nil t)
 
-  (process-put vterm--process :fakecygpty-p t)
+  (when (eq system-type 'windows-nt)
+    (process-put vterm--process :fakecygpty-p t))
   (vterm--set-pty-name vterm--term (process-tty-name vterm--process))
   (process-put vterm--process 'adjust-window-size-function
                #'vterm--window-adjust-process-window-size)
